31bc2da12b9aaf9663921585d395d12f53ca02db
ondrej tichavsky <ondrej.tichavsky@quantasoft.com>
2026-03-05 09:26:46 +0100

    finding scripts fix

--- start-app.bat
+++ start-app.bat
@@ -11,9 +11,27 @@ echo [2/2] Waiting 3 seconds for backend to start...
 timeout /t 3 /nobreak > nul
 
 echo [3/3] Starting Quick Launcher...
-cd user_data
-start "" "launcher_gui.ahk"
-cd ..
+rem Find AutoHotkey executable
+set "AHK_EXE="
+if exist "%LOCALAPPDATA%\Programs\AutoHotkey\v2\AutoHotkey64.exe" (
+    set "AHK_EXE=%LOCALAPPDATA%\Programs\AutoHotkey\v2\AutoHotkey64.exe"
+) else if exist "%LOCALAPPDATA%\Programs\AutoHotkey\v2\AutoHotkey32.exe" (
+    set "AHK_EXE=%LOCALAPPDATA%\Programs\AutoHotkey\v2\AutoHotkey32.exe"
+) else if exist "%ProgramFiles%\AutoHotkey\v2\AutoHotkey64.exe" (
+    set "AHK_EXE=%ProgramFiles%\AutoHotkey\v2\AutoHotkey64.exe"
+) else if exist "%ProgramFiles%\AutoHotkey\v2\AutoHotkey32.exe" (
+    set "AHK_EXE=%ProgramFiles%\AutoHotkey\v2\AutoHotkey32.exe"
+) else if exist "%ProgramFiles%\AutoHotkey\AutoHotkey.exe" (
+    set "AHK_EXE=%ProgramFiles%\AutoHotkey\AutoHotkey.exe"
+)
+if not "%AHK_EXE%"=="" (
+    cd user_data
+    start "" "%AHK_EXE%" "launcher_gui.ahk"
+    cd ..
+    echo Launcher started with: %AHK_EXE%
+) else (
+    echo WARNING: AutoHotkey not found. Ctrl+F4 launcher will not be available.
+)
 
 echo Starting Frontend Application...
 npm start
